537e6f3a16829ca5a58a2bc6ef60942c2a8a6ed4
bangga2004 <96578070+bangga2004@users.noreply.github.com>
2022-03-01 20:37:33 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -87,7 +87,7 @@ exports.notNum = (q) => {
 }
 exports.menunya = (salam, pushname) =>{
 	return`Hai kak ${pushname} 👋 selamat ${salam} , 
-saya BRIYAN BOT, bot ini adalah Beta Multi-Device Whatsapp. 
+saya  𝘽𝘼𝙍𝙊𝙉, bot ini adalah Beta Multi-Device Whatsapp. 
 Jika kamu menemukan semacam bug atau kesalahan mohon dimaklumi dulu ya, 
 lapor owner agar segera di perbaiki 🙏      
 `
@@ -95,7 +95,7 @@ lapor owner agar segera di perbaiki 🙏
 exports.listMenu = (time, salam, pushname, prefix) => {
     return `*Selamat ${salam} ${pushname}*
 *⌚Time Server : ${time}*
-*📚 List-Menu BRIYAN BOT :*
+*📚 List-Menu  𝘽𝘼𝙍𝙊𝙉 :*
 
 ╭─❒ 「 Bot Info 」 
 ├ ${prefix}owner
