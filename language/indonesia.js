3715bdfc1d779f922120e71116b6f94d210dccf9
bangga2004 <96578070+bangga2004@users.noreply.github.com>
2022-06-04 22:56:24 +0700

    Update indonesia.js

--- language/indonesia.js
+++ language/indonesia.js
@@ -87,7 +87,7 @@ exports.notNum = (q) => {
 }
 exports.menunya = (salam, pushname) =>{
 	return`Hai kak ${pushname} 👋 selamat ${salam} , 
-saya  𝘽𝘼𝙍𝙊𝙉, bot ini adalah Beta Multi-Device Whatsapp. 
+saya  ResyaBotZz, bot ini adalah Beta Multi-Device Whatsapp. 
 Jika kamu menemukan semacam bug atau kesalahan mohon dimaklumi dulu ya, 
 lapor owner agar segera di perbaiki 🙏      
 `
@@ -95,7 +95,7 @@ lapor owner agar segera di perbaiki 🙏
 exports.listMenu = (time, salam, pushname, prefix) => {
     return `*Selamat ${salam} ${pushname}*
 *⌚Time Server : ${time}*
-*📚 List-Menu  𝘽𝘼𝙍𝙊𝙉 :*
+*📚 List-Menu  ResyaBotZz :*
 
 ╭─❒ 「 Bot Info 」 
 ├ ${prefix}owner
@@ -1281,7 +1281,7 @@ return`╭─❒ 「 Random Cewe 」
 
 exports.tqto = () =>{
 	return`╭─❒ 「 TqTo 」 
-├ Baron Store
+├ ResyaBotZz
 ├ Penyedia Module
 ╰❒ And All Support
 `
